6f729145a0191135ca3163010af584a7752ffa86
beemusy <beemusy@gmail.com>
2023-05-10 23:53:06 +0200

    task13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,16 @@
+/*
+ * File: 13-insert_number.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * insert_number -  inserts a number into a sorted singly linked list
+ *
+ * Return: address of the new node
+*/
+listint_t *insert_node(listint_t **head, int number);
+{
