50faac2aab44e2f9a9065942caa23804d1051c81
beemusy <beemusy@gmail.com>
2023-05-11 15:34:10 +0200

    cal errors

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
 # import functions from the file calculator_1.py
-
-from calculator_1 import add, sub, mul, div
-
-# define the values a and b to 10 and 5
-
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
