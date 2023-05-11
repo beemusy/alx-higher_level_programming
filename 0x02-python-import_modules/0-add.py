bc3b92763c7b3dff019f9822d00df076ead4a583
beemusy <beemusy@gmail.com>
2023-05-11 11:33:12 +0200

    pycodestyle

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-#Import the add function from module add-0
+# Import the add function from module add-0
 
 from add_0 import add
 
-#initialise variables a and b
+# initialise variables a and b
 a = 1
 b = 2
 
-#call add function
+# call add function
 result = add(a, b)
 
-#print the result
-print ("{} + {} = {}".format(a, b, result))
+# print the result
+print("{} + {} = {}".format(a, b, result))
