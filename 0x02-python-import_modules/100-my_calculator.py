292f29a64d622e70e0b6d6f727a4b764fce622bf
beemusy <beemusy@gmail.com>
2023-05-12 15:04:29 +0200

    task 100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+from sys import argv
+from calculator_1 import add, sub, mul, div
+
+if __file__ == "__main__":
+    numOfArg = len(argv)
+
+    if numOfArg != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    a = int(argv[1])
+    operator = int(argv[2])
+    b = int(argv[3])
+
+    if operator == "+":
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif operator == "-":
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif operator == "*":
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif operator == "/":
+        print("{:d} / {:d} = {:d}".format(a, b, add(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
