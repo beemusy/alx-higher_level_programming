db4d620b8e7ce2e844ab51d3cf3bbc705d686657
beemusy <beemusy@gmail.com>
2023-05-15 22:42:44 +0200

    10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    result_list = []
+    for val in my_list:
+        is_divisible = (val % 2 == 0)
+        result_list.append(is_divisible)
+    return(result_list)
