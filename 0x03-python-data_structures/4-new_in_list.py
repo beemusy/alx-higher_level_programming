9d37d00bde4f9af2e5e2c902ad3f06fadfd3e608
beemusy <beemusy@gmail.com>
2023-05-15 16:23:39 +0200

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     # checking if idx is negative or out of range
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return(my_list)
     new_list = my_list[:]
     new_list[idx] = element
