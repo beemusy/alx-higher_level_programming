bd5f1f999feeeb36f95fcba00574330ffdfc481d
beemusy <beemusy@gmail.com>
2023-05-15 22:36:24 +0200

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return(None)
+    max_val = my_list[0]
+    for val in my_list[1:]:
+        if val > max_val:
+            max_val = val
+    return(max_val)
