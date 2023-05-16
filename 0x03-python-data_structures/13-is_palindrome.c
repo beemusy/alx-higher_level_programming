1ce1ad27d5629e7050aa1474122ec9c875149fbf
beemusy <beemusy@gmail.com>
2023-05-16 21:34:27 +0200

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,22 @@
+/*
+ * File: 13-is_palindrome.c
+ * Auth: Blessings Moyo
+ */
+
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if elements in a
+ * linked list are a palindrome
+ *
+ * @listint_t: linked list
+ * @head: pointer to header of linked list
+ * Return: 0 if its not a palindrome or 1 if palindrome
+ */
+int is_palindrome(listint_t **head)
+{
+	if(*head == NULL || (*head)->next == NULL)
+		return(1);
+	/*finding the middle of the list*/
+}
