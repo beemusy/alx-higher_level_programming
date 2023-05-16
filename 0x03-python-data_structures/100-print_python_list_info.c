e2175d71c7dc786bdd75f513f40c3e8db1bb57e4
beemusy <beemusy@gmail.com>
2023-05-16 22:25:37 +0200

    task 100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,18 @@
+/*
+ * File: 100-print_python_list_info.c
+ * Auth: Blessings Moyo
+ */
+
+#include <python.h>
+
+/**
+ * print_python_list_info - C function that
+ * prints some basic info about Python lists.
+ *
+ * @p: pointer to a PyObject
+ * Return: Always 0
+ */
+void print_python_list_info(PyObject *p)
+{
+	return (0);
+}
