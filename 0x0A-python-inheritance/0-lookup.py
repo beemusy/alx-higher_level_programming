b5a21fa41b7f852098ce4106c6b169e351d91edc
beemusy <beemusy@gmail.com>
2023-06-13 00:15:45 +0200

    0-lookup.py

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+0-lookup.py
+function that returns the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ tongai  """
+    return dir(obj)
