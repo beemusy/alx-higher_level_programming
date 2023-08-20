0747eab6867c17d5f1fdd9b45b1ce3256111ccfb
beemusy <beemusy@gmail.com>
2023-08-20 20:48:48 +0200

    relational mapping

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}{}".format(chr(i), chr(i-32) if i % 2 == 0 else ''), end='')
+    print("{}".format(chr(i), chr(i-32) if i % 2 == 0 else ''), end='')
---
+++ 0x01-python-if_else_loops_functions/test.py
@@ -0,0 +1,5 @@
+file_path = "path/to/your/file.txt"
+
+with open(file_path, "r") as file:
+    file_contents = file.read()
+    print(file_contents)
